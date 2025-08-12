b55e8843059c629b5bb2910e676e0f5d27b6dc26
Jorge Castillo <schorts@outlook.com>
2025-08-12 14:21:31 -0600

    feat: Add board routes

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,14 @@
 class BoardsController < ApplicationController
   def index
+    boards_all_getter = ::Boards::GetAll::BoardsAllGetter.new
+    @boards = boards_all_getter.get_all
+  end
+
+  def create
+    board_creator = ::Boards::Create::BoardCreator.new
+    board = board_creator.create
+
+    redirect_to board_path(id: board.id)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
-  resources :boards, only: [:index, :show]
+  mount Sidekiq::Web => "/sidekiq"
+
+  resources :boards, only: [:show, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
